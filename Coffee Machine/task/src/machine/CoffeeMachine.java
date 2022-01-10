8bb2d8a15f2e7b73e0a29ff4ec6cb49f95089872
Maksim Korelov <korelov@gmail.com>
2022-01-11 00:08:43 +0300

    Stage 3 / 6: Estimate the number of servings done

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,34 +4,48 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 
-    final static Scanner scanner = new Scanner(System.in);
-    final static int WATER = 200;
-    final static int MILK = 50;
-    final static int COFFEE_BEANS = 15;
+    static Scanner scanner = new Scanner(System.in);
+    static final int WATER = 200;
+    static final int MILK = 50;
+    static final int COFFEE_BEANS = 15;
+    static int waterCoffeeMachineHas;
+    static int milkCoffeeMachineHas;
+    static int coffeeBeansCoffeeMachineHas;
+    static int cupsOfCoffee;
 
+    public static void estimateNumberServings() {
 
-    public static void ingredientCalculator() {
+        System.out.println("Write how many ml of water the coffee machine has:");
+        waterCoffeeMachineHas = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        milkCoffeeMachineHas = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        coffeeBeansCoffeeMachineHas = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        cupsOfCoffee = scanner.nextInt();
+    }
 
-        System.out.print("Write how many cups of coffee you will need: \n" +
-                "> ");
-        int cupsOfCoffee = scanner.nextInt();
-        System.out.printf("For %d cups of coffee you will need:%n", cupsOfCoffee);
-        System.out.printf("%d ml of water%n", cupsOfCoffee * WATER);
-        System.out.printf("%d ml of milk%n", cupsOfCoffee * MILK);
-        System.out.printf("%d g of coffee beans%n", cupsOfCoffee * COFFEE_BEANS);
+    public static void showAmountCoffee() {
+
+        int[] arr = {waterCoffeeMachineHas / WATER, milkCoffeeMachineHas / MILK, coffeeBeansCoffeeMachineHas / COFFEE_BEANS};
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        if (cupsOfCoffee == min) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsOfCoffee > min) {
+            System.out.printf("No, I can make only %d cup(s) of coffee", min);
+        } else if (cupsOfCoffee < min) {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", min - cupsOfCoffee);
+        }
     }
 
     public static void main(String[] args) {
 
-        ingredientCalculator();
-
-//        System.out.println("Starting to make a coffee\n" +
-//                "Grinding coffee beans\n" +
-//                "Boiling water\n" +
-//                "Mixing boiled water with crushed coffee beans\n" +
-//                "Pouring coffee into the cup\n" +
-//                "Pouring some milk into the cup\n" +
-//                "Coffee is ready!");
-
+        estimateNumberServings();
+        showAmountCoffee();
     }
 }
